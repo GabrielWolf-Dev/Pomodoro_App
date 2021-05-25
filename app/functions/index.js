9360ba7e1b7f0c3cf6968a9f9f323f6352a83ef8
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-05-24 22:35:39 -0300

    Update
    header of website was finished

---
+++ app/functions/index.js
@@ -0,0 +1,5 @@
+import menuMobile from './menuMobile.js';
+
+window.onload = () => {
+    menuMobile();
+}
---
+++ app/functions/menuMobile.js
@@ -0,0 +1,19 @@
+export default function menuMobile() {
+    const $ = document.querySelector.bind(document);
+    const btnMenu = $('#btnMenu');
+    const menu = $('#menu');
+
+    btnMenu.addEventListener('click', (event) => {
+        const activeMenu = btnMenu.classList.contains('menu-mobile__btn--active');
+
+        btnMenu.classList.toggle('menu-mobile__btn--active');
+        menu.classList.toggle('menu-mobile__menu--active');
+
+        event.currentTarget.setAttribute('aria-expended', activeMenu);
+        if(activeMenu) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        }
+    });
+}
