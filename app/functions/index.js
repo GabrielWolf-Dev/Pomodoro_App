46fb001ba8a26d3949cd2cac37923266ddc99b04
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-05-29 21:59:50 -0300

    Update
    Adding the smooth scroll effect, and starting the todo list style.

--- app/functions/index.js
+++ app/functions/index.js
@@ -1,5 +1,9 @@
 import menuMobile from './menuMobile.js';
+import smoothScroll from './smoothScroll.js';
+import showTodo from './showTodo.js';
 
 window.onload = () => {
     menuMobile();
+    smoothScroll();
+    showTodo();
 }
---
+++ app/functions/showTodo.js
@@ -0,0 +1,20 @@
+export default function showTodo() {
+    const $ = document.querySelector.bind(document);
+    const todo = $('.todo-list');
+    const btnOpen = $('.app__btn');
+    const btnClose = $('.todo-list__btn-close');
+    const bg = $('.bg-black');
+
+    btnOpen.addEventListener('click', () => {
+        todo.classList.add('todo-list--active');
+        bg.classList.add('bg-black--active');
+    });
+
+    btnClose.addEventListener('click', closeMenu);
+    bg.addEventListener('click', closeMenu);
+
+    function closeMenu() {
+        todo.classList.remove('todo-list--active');
+        bg.classList.remove('bg-black--active');
+    }
+}
